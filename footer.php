f5df3af674dba50be78e76c99b9ce94431397e0c
Andy Strange <code@grit-oyster.co.uk>
2020-05-27 11:17:42 +0100

    Improved grid layout for header and footer

--- footer.php
+++ footer.php
@@ -13,21 +13,36 @@
 </div><!-- .row -->
 </div><!-- .container -->
 </div><!-- #content -->
-	<footer id="colophon" class="site-footer">
-		<div class="container pt-3 pb-3">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'hounslow-intranet' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'hounslow-intranet' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'hounslow-intranet' ), 'hounslow-intranet', '<a href="https://hounslow.digital/">London Borough of Hounslow</a>' );
-				?>
-		</div><!-- .site-info -->
+	<footer id="colophon" class="site-footer bg-secondary">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm">
+		      One of three columns
+		    </div>
+		    <div class="col-sm">
+		      One of three columns
+		    </div>
+		    <div class="col-sm">
+		      One of three columns
+		    </div>
+			</div>
+			<div class="row">
+				<div class="col text-center">
+					<div class="site-info">
+						<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'hounslow-intranet' ) ); ?>">
+							<?php
+							/* translators: %s: CMS name, i.e. WordPress. */
+							printf( esc_html__( 'Proudly powered by %s', 'hounslow-intranet' ), 'WordPress' );
+							?>
+						</a>
+						<span class="sep"> | </span>
+							<?php
+							/* translators: 1: Theme name, 2: Theme author. */
+							printf( esc_html__( 'Theme: %1$s by %2$s.', 'hounslow-intranet' ), 'hounslow-intranet', '<a href="https://hounslow.digital/">London Borough of Hounslow</a>' );
+							?>
+					</div><!-- .site-info -->
+				</div>
+			</div>
 		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- header.php
+++ header.php
@@ -25,38 +25,53 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'hounslow-intranet' ); ?></a>
 
-	<header id="masthead" class="site-header">
+	<header id="masthead" class="site-header bg-primary">
 		<div class="container">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$hounslow_intranet_description = get_bloginfo( 'description', 'display' );
-			if ( $hounslow_intranet_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $hounslow_intranet_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'hounslow-intranet' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
+			<div class="row">
+				<div class="col-sm-8">
+					<div class="site-branding">
+						<?php
+						the_custom_logo();
+						if ( is_front_page() && is_home() ) :
+							?>
+							<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+							<?php
+						else :
+							?>
+							<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+							<?php
+						endif;
+						$hounslow_intranet_description = get_bloginfo( 'description', 'display' );
+						if ( $hounslow_intranet_description || is_customize_preview() ) :
+							?>
+							<p class="site-description"><?php echo $hounslow_intranet_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+						<?php endif; ?>
+					</div><!-- .site-branding -->
+				</div>
+				<div class="col-sm-4 text-sm-right">
+					SEARCH BOX
+				</div>
+			</div>
+			<div class="row">
+				<div class="col text-sm-right">
+					UTILITIES NAV
+				</div>
+			</div>
+			<div class="row">
+				<div class="col">
+					<nav id="site-navigation" class="main-navigation">
+						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'hounslow-intranet' ); ?></button>
+						<?php
+						wp_nav_menu(
+							array(
+								'theme_location' => 'menu-1',
+								'menu_id'        => 'primary-menu',
+							)
+						);
+						?>
+					</nav><!-- #site-navigation -->
+				</div>
+			</div>
 	</div>
 	</header><!-- #masthead -->
 	<div id="content" class="site-content">
