e63bf8db5c55b9820e80dd1b6870544ab0ff1c38
Andy Strange <code@grit-oyster.co.uk>
2020-05-29 11:38:12 +0100

    Removed secondary menu

--- functions.php
+++ functions.php
@@ -51,7 +51,6 @@ if ( ! function_exists( 'hounslow_intranet_setup' ) ) :
 		register_nav_menus(
 			array(
 				'menu-1' => esc_html__( 'Primary', 'hounslow-intranet' ),
-				'menu-2' => esc_html__( 'Secondary', 'hounslow-intranet' ),
 			)
 		);
 
