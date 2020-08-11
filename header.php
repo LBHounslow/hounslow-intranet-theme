1e6bdc401a80206c6c6c1738c5e9dd59fd720334
Andy Strange <code@grit-oyster.co.uk>
2020-08-11 11:58:41 +0100

    Removed the search box

--- header.php
+++ header.php
@@ -32,10 +32,6 @@
 				  <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
 				</svg> | Hounslow Intranet
 			</a>
-			<form class="form-inline">
-		    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-		    <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Search</button>
-		  </form>
 		</nav>
 		<div class="container">
 			<nav id="site-branding" class="navbar">
