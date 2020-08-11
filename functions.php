e8f02032e5c75168c74d2f9aed38fdf6ebb94ab4
Andy Strange <code@grit-oyster.co.uk>
2020-08-11 11:38:42 +0100

    Comment out nav menu registration

--- functions.php
+++ functions.php
@@ -48,11 +48,11 @@ if ( ! function_exists( 'hounslow_intranet_setup' ) ) :
 		add_theme_support( 'post-thumbnails' );
 
 		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus(
+		/*register_nav_menus(
 			array(
 				'menu-1' => esc_html__( 'Primary', 'hounslow-intranet' ),
 			)
-		);
+		);*/
 
 		/*
 		 * Switch default core markup for search form, comment form, and comments
