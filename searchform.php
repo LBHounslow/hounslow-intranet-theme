680e9e0fbdfb21a4506b05de615a8bb51c7325fc
Andy Strange <code@grit-oyster.co.uk>
2020-10-07 19:17:28 +0100

    Implemented Swiftype Search

--- searchform.php
+++ searchform.php
@@ -1,8 +1,8 @@
-<form role="search" method="get" class="form search-form" action="<?php esc_url( home_url( '/' ) ); ?>">
+<form role="search" method="get" class="form search-form" action="/search-results/">
   <div class="input-group">
-      <input name="s" type="text" class="form-control" placeholder="Search in this site">
+      <input name="s" type="text" class="st-default-search-input" placeholder="Search the Intranet">
       <span class="input-group-btn">
         <button type="submit" value="Search" class="btn btn-primary" type="button"><i class="fa fa-search" aria-hidden="true"></i>&nbsp;</button>
       </span>
-    </div>
+  </div>
 </form>
