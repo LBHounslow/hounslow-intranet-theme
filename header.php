38ccc42f1383a6276e6261ba703c665886db01d9
Andy Strange <code@grit-oyster.co.uk>
2021-03-31 12:53:11 +0100

    Updated link to new-style.css

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" id="custom-css" href="/wp-content/themes/hounslow-intranet-theme-v2-test/css/new-style.css?ver=4" type="text/css" media="all">
+	<link rel="stylesheet" id="custom-css" href="/wp-content/themes/hounslow-intranet-theme/css/new-style.css?ver=4" type="text/css" media="all">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link rel="stylesheet"
   href="https://fonts.googleapis.com/css?family=Nunito+Sans">
