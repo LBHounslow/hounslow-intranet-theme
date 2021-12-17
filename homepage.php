521838d664f66686bca0215dfe2894714112e31f
Andy Strange <andy.strange@hounslow.gov.uk>
2021-12-17 15:51:16 +0000

    Formatting changes

--- homepage.php
+++ homepage.php
@@ -1,34 +1,25 @@
-<?php
-/**
- * The home template file
-
-* TEMPLATE NAME: Homepage
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package LBH_Intranet
- */
-
+<?php
+/**
+ * The home template file
+ * TEMPLATE NAME: Homepage
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package LBH_Intranet
+ */
 get_header();
 get_sidebar();
-?>
-
-
-        <!-- Page Content  -->
-        <div id="content">
-
-
-            <?php get_template_part('templates/homepage-template', 'homepage'); ?>
-
-
-        </div>
-
-
-    </div>
-</body><?php
-get_footer();
+?>
+<!-- Page Content  -->
+<div id="content">
+  <main id="primary" class="site-main">
+    <?php get_template_part( 'templates/homepage-template', 'homepage' ); ?>
+  </main><!-- #primary .site-main -->
+</div><!-- #content -->
+<?php
+get_footer();
--- template-parts/header-banner.php
+++ template-parts/header-banner.php
@@ -1,24 +1,14 @@
 <div class="status-bar">
-<btn class="btn selector"><?php echo do_shortcode('[gtranslate]'); ?></btn>
-<a id="desktop" class="btn btn-light status-btn"  href="/feedback">Leave Feedback</a>
-<a id="desktop" class="btn btn-light status-btn"  href="/submit-a-new-intranet-page">Add a Page</a>
-<a id="desktop" class="btn btn-light status-btn"  href="/event-submission-form/?action=edit">Add an Event</a>
-
-<?php $value = rwmb_meta( 'lbh_status_radio');
-if ( 'working' == $value ) { ?>
-
- <a id="desktop" style="color:white" href="/system-status"><button class="btn btn-success status-btn"><strong>See Status</strong></button></a>
-
-<?php } elseif ( 'issues' == $value ) { ?>
-
- <a id="desktop" style="color:white" href="/system-status"><button class="btn btn-warning status-btn"><strong>See Status</strong></button></a>
-
-<?php } elseif ( 'down' == $value ) { ?>
- 
- <a id="desktop" style="color:white" href="/system-status"><button class="btn btn-danger status-btn"><strong>See Status</strong></button></a>
-
-<?php } ?>
-
-
-
+  <btn class="btn selector"><?php echo do_shortcode('[gtranslate]'); ?></btn>
+  <a id="desktop" class="btn btn-light status-btn"  href="/feedback">Leave Feedback</a>
+  <a id="desktop" class="btn btn-light status-btn"  href="/submit-a-new-intranet-page">Add a Page</a>
+  <a id="desktop" class="btn btn-light status-btn"  href="/event-submission-form/?action=edit">Add an Event</a>
+  <?php $value = rwmb_meta( 'lbh_status_radio');
+  if ( 'working' == $value ) { ?>
+    <a id="desktop" style="color:white" href="/system-status"><button class="btn btn-success status-btn"><strong>See Status</strong></button></a>
+  <?php } elseif ( 'issues' == $value ) { ?>
+    <a id="desktop" style="color:white" href="/system-status"><button class="btn btn-warning status-btn"><strong>See Status</strong></button></a>
+  <?php } elseif ( 'down' == $value ) { ?>
+    <a id="desktop" style="color:white" href="/system-status"><button class="btn btn-danger status-btn"><strong>See Status</strong></button></a>
+  <?php } ?>
 </div>
