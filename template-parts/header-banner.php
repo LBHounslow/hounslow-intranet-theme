53b972fa3cd0f508ba8d6a2e687fce77113d025d
Andy Strange <andy.strange@hounslow.gov.uk>
2022-01-27 15:59:31 +0000

    Removed the Leave Feedback button from the header banner

--- template-parts/header-banner.php
+++ template-parts/header-banner.php
@@ -1,6 +1,5 @@
 <div class="status-bar">
   <btn class="btn selector"><?php echo do_shortcode('[gtranslate]'); ?></btn>
-  <a id="desktop" class="btn btn-light status-btn"  href="/feedback">Leave Feedback</a>
   <a id="desktop" class="btn btn-light status-btn"  href="/submit-a-new-intranet-page">Add a Page</a>
   <a id="desktop" class="btn btn-light status-btn"  href="/event-submission-form/?action=edit">Add an Event</a>
   <?php $value = rwmb_meta( 'lbh_status_radio');
