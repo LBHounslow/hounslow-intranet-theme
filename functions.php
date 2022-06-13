c01ea7f960db0d05bb2b06c524b3684bffe8a4e2
Andy Strange <code@grit-oyster.co.uk>
2022-06-13 13:54:08 +0100

    Remove PHPCS Ignore Around $content-width

--- functions.php
+++ functions.php
@@ -118,9 +118,6 @@ add_action( 'after_setup_theme', 'register_navwalker' );
  * @global int $content_width
  */
 function hounslow_intranet_content_width() {
-	// This variable is intended to be overruled from themes.
-	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
-	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
 	$GLOBALS['content_width'] = apply_filters( 'hounslow_intranet_content_width', 640 );
 }
 add_action( 'after_setup_theme', 'hounslow_intranet_content_width', 0 );
