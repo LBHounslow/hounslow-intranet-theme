31a65cbf76ed431bb88ed5a8ea282a2f1f8b0d53
Andy Strange <code@grit-oyster.co.uk>
2022-06-13 13:35:19 +0100

    Comment out featured engage posts carousel

--- templates/homepage-template.php
+++ templates/homepage-template.php
@@ -61,99 +61,63 @@ if ( function_exists('hounslow_intranet_network_news_query_banner')  ) {
 ?>
 
 
-           <div class="row">
+<div class="row">
+  <div class="col-lg-9">
+    <div class="row outer">
+      <?php
+      $loop = false;
+      if ( function_exists('hounslow_intranet_network_news_query_featured')  ) {
+        $loop = hounslow_intranet_network_news_query_featured($news_site_id);
+        if ($loop->have_posts()) :
+            while ($loop->have_posts()) : $loop->the_post();
+            $news_post = get_post();
+            switch_to_blog( $news_site_id );
+            $news_post_thumbnail_url = get_the_post_thumbnail_url( $news_post->ID );
+            $news_post_permalink = get_the_permalink( $news_post->ID );
+            $news_post_video = rwmb_meta( 'lbh_featured_video' );
+            $news_post_video_value = rwmb_get_value( 'lbh_featured_video' );
+            restore_current_blog(); ?>
 
-                <div class="col-lg-9">
-
-
-
-              <div class="row outer">
-
-
-<?php
-
-$loop = false;
-if ( function_exists('hounslow_intranet_network_news_query_featured')  ) {
-  $loop = hounslow_intranet_network_news_query_featured($news_site_id);
-  if ($loop->have_posts()) :
-      while ($loop->have_posts()) : $loop->the_post();
-      $news_post = get_post();
-      switch_to_blog( $news_site_id );
-      $news_post_thumbnail_url = get_the_post_thumbnail_url( $news_post->ID );
-      $news_post_permalink = get_the_permalink( $news_post->ID );
-      $news_post_video = rwmb_meta( 'lbh_featured_video' );
-      $news_post_video_value = rwmb_get_value( 'lbh_featured_video' );
-      restore_current_blog();
-      ?>
-
-
-  			<div class="col-lg-6" style="padding:0px;">
-                  <div class="inner">
-
-<?php if ( $news_post_video_value ): ?>
-
-  <?php echo $news_post_video; ?>
-
-<?php else: ?>
-
- <div style="background:url('<?php echo $news_post_thumbnail_url; ?>');height:300px;background-size:cover;background-position:center;">
+            <div class="col-lg-6" style="padding:0px;">
+              <div class="inner">
+                <?php if ( $news_post_video_value ):
+                  echo $news_post_video;
+                else: ?>
+                <div style="background:url('<?php echo $news_post_thumbnail_url; ?>');height:300px;background-size:cover;background-position:center;">
                 </div>
+               <?php endif; ?>
+             </div>
+           </div>
+           <div class="col-lg-6 inner">
+             <h5><?php the_title(); ?></h5>
+             <p><?php the_time(get_option('date_format')); ?></p>
+             <?php the_excerpt(); ?>
+             <a style="color:white;"href="<?php echo $news_post_permalink; ?>"><button class="btn btn-dark">Read More</button></a>
+           </div>
+         <?php endwhile;
+       endif;
+       wp_reset_postdata();
+     } ?>
+       <div class="featured-border">
+       </div>
+     </div>
+   </div>
 
-<?php endif; ?>
-
-                  </div>
-
-
-
-
-              </div>
-
-                      <div class="col-lg-6 inner">
-  					 <h5><?php the_title(); ?></h5>
-                       <p><?php the_time(get_option('date_format')); ?></p>
-                       <?php the_excerpt(); ?>
-                       <a style="color:white;"href="<?php echo $news_post_permalink; ?>"><button class="btn btn-dark">Read More</button></a>
-  					</div>
-
-              <?php
-
-
-
-      endwhile;
-  endif;
-  wp_reset_postdata();
-}
-
-?>
- <div class="featured-border">
+  <div class="col-lg-3 outer">
+    <div class="title" style="margin-left:8px;">
+    </div>
+    <div class="">
+      <div class="inner">
+  				<?php echo EM_Calendar::output(array('full'=>0, 'long_events'=>1)); ?>
+      </div>
+      <div class="inner-feature shadow">
+        <a style="color:white" href="/events"><button class="btn btn-dark">See All Events</button></a>
+      </div>
+    </div>
+  </div>
 </div>
-                </div>
-				</div>
 
-
-				<div class="col-lg-3 outer">
-				<div class="title" style="margin-left:8px;">
-
-				</div>
-
-			<div class="">
-
-					<div class="inner">
-				<?php echo EM_Calendar::output(array('full'=>0, 'long_events'=>1)); ?>
-
-
-                    </div>
-                    <div class="inner-feature shadow">
-                    <a style="color:white" href="/events"><button class="btn btn-dark">See All Events</button></a>
-
-                    </div>
-                    </div>
-				</div>
-
-
-
-</div>
-<?php get_template_part('templates/topics-carousel'); ?>
+<?php //get_template_part('templates/topics-carousel'); ?>
 
 <div class="row">
 <div class="col-lg-9">
