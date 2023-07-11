6a4ccbc74b5e0beec8c0ef3933c400051f90d053
Andy Strange <andy.strange@hounslow.gov.uk>
2023-07-11 15:56:21 +0100

    Remove header-banner template part

--- header.php
+++ header.php
@@ -23,7 +23,11 @@
 <body <?php body_class(); ?>>
 	<div id="before-page" data-swiftype-index="false">
 		<?php wp_body_open(); ?>
-		<?php get_template_part('template-parts/header-banner', 'menu'); ?>
+		<div class="status-bar">
+			<btn class="btn selector"><?php echo do_shortcode('[gtranslate]'); ?></btn>
+			<a id="desktop" class="btn btn-light status-btn" href="/submit-a-new-intranet-page">Add a Page</a>
+			<a id="desktop" class="btn btn-light status-btn" href="/event-submission-form/?action=edit">Add an Event</a>
+		</div>
 	</div>
 	<div id="page" class="site">
 		<a class="visually-hidden-focusable" href="#primary" data-swiftype-index="false"><?php esc_html_e('Skip to content', 'hounslow-intranet-theme'); ?></a>
--- template-parts/header-banner.php
+++
@@ -1,5 +0,0 @@
-<div class="status-bar">
-  <btn class="btn selector"><?php echo do_shortcode('[gtranslate]'); ?></btn>
-  <a id="desktop" class="btn btn-light status-btn"  href="/submit-a-new-intranet-page">Add a Page</a>
-  <a id="desktop" class="btn btn-light status-btn"  href="/event-submission-form/?action=edit">Add an Event</a>
-</div>
