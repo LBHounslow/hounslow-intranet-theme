35f530736c7a748b2384a33ac4222a07134201d1
Andy Strange <andy.strange@hounslow.gov.uk>
2023-07-19 16:07:48 +0100

    removed #before-page and #page divs

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -9,63 +10,70 @@
  * @package Hounslow_Intranet
  */
 ?>
-		</div><!-- .wrapper -->
-		<footer id="colophon" class="site-footer bg-secondary2 px-3" data-swiftype-index="false">
-			<div class="container-fluid">
-				<div class="row">
-					<div id="footer-column-one" class="widget-area col-sm">
-						<?php hounslow_intranet_multisite_sidebar( 'sidebar-fc1' ) ?>
-			    </div>
-					<div id="footer-column-two" class="widget-area col-sm">
-						<?php hounslow_intranet_multisite_sidebar( 'sidebar-fc2' ) ?>
-			    </div>
-					<div id="footer-column-three" class="widget-area col-sm">
-						<?php hounslow_intranet_multisite_sidebar( 'sidebar-fc3' ) ?>
-			    </div>
-					<div id="footer-column-four" class="widget-area col-sm">
-						<?php hounslow_intranet_multisite_sidebar( 'sidebar-fc4' ) ?>
-			    </div>
-				</div><!-- .row -->
-				<div class="row justify-content-center">
-					<div class="col col-md-8 col-lg-6">
-						<?php if ( function_exists('hounslow_intranet_network_nav_menu') && is_user_logged_in() ) {
-						$social_menu_args = array(
-								'theme_location'    => 'social',
-								'depth'          => 1,
-								'container'      => false,
-								'container_class'=> '',
-								'menu_class'     => 'nav justify-content-center',
-								'fallback_cb'    => 'WP_Bootstrap_Navwalker::fallback',
-								'walker'         => new WP_Bootstrap_Navwalker(),
-						);
-						hounslow_intranet_network_nav_menu( $social_menu_args );
-					} ?>
-				  </div><!-- .col -->
-				</div><!-- .row -->
-				<div class="row">
-					<div class="col text-center">
-						<div class="site-info">Hounslow Intranet &#169; Copyright London Borough of Hounslow</div><!-- .site-info -->
-					</div><!-- .col -->
-				</div><!-- .row -->
-			</div><!-- .container -->
-		</footer><!-- #colophon -->
-	</div><!-- #page -->
-	<script type="text/javascript">
-		jQuery( document ).ready(function( $ ) {
-		  $('#sidebarCollapse').on('click', function () {
-		      $('#sidebar').toggleClass('active');
-		  });
+</div><!-- .wrapper -->
+<footer id="colophon" class="site-footer bg-secondary2 px-3" data-swiftype-index="false">
+	<div class="container-fluid">
+		<div class="row">
+			<div id="footer-column-one" class="widget-area col-sm">
+				<?php hounslow_intranet_multisite_sidebar('sidebar-fc1') ?>
+			</div>
+			<div id="footer-column-two" class="widget-area col-sm">
+				<?php hounslow_intranet_multisite_sidebar('sidebar-fc2') ?>
+			</div>
+			<div id="footer-column-three" class="widget-area col-sm">
+				<?php hounslow_intranet_multisite_sidebar('sidebar-fc3') ?>
+			</div>
+			<div id="footer-column-four" class="widget-area col-sm">
+				<?php hounslow_intranet_multisite_sidebar('sidebar-fc4') ?>
+			</div>
+		</div><!-- .row -->
+		<div class="row justify-content-center">
+			<div class="col col-md-8 col-lg-6">
+				<?php if (function_exists('hounslow_intranet_network_nav_menu') && is_user_logged_in()) {
+					$social_menu_args = array(
+						'theme_location'    => 'social',
+						'depth'          => 1,
+						'container'      => false,
+						'container_class' => '',
+						'menu_class'     => 'nav justify-content-center',
+						'fallback_cb'    => 'WP_Bootstrap_Navwalker::fallback',
+						'walker'         => new WP_Bootstrap_Navwalker(),
+					);
+					hounslow_intranet_network_nav_menu($social_menu_args);
+				} ?>
+			</div><!-- .col -->
+		</div><!-- .row -->
+		<div class="row">
+			<div class="col text-center">
+				<div class="site-info">Hounslow Intranet &#169; Copyright London Borough of Hounslow</div><!-- .site-info -->
+			</div><!-- .col -->
+		</div><!-- .row -->
+	</div><!-- .container -->
+</footer><!-- #colophon -->
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$('#sidebarCollapse').on('click', function() {
+			$('#sidebar').toggleClass('active');
 		});
-	</script>
-	<!-- Swiftype Search -->
-	<script type="text/javascript">
-		(function(w,d,t,u,n,s,e){w['SwiftypeObject']=n;w[n]=w[n]||function(){
-	  	(w[n].q=w[n].q||[]).push(arguments);};s=d.createElement(t);
-	  	e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.insertBefore(s,e);
-	  })(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
+	});
+</script>
+<!-- Swiftype Search -->
+<script type="text/javascript">
+	(function(w, d, t, u, n, s, e) {
+		w['SwiftypeObject'] = n;
+		w[n] = w[n] || function() {
+			(w[n].q = w[n].q || []).push(arguments);
+		};
+		s = d.createElement(t);
+		e = d.getElementsByTagName(t)[0];
+		s.async = 1;
+		s.src = u;
+		e.parentNode.insertBefore(s, e);
+	})(window, document, 'script', '//s.swiftypecdn.com/install/v2/st.js', '_st');
 
-		_st('install','_DkzPVuz5eyykUsibtix','2.0.0');
-	</script>
-	<?php wp_footer(); ?>
+	_st('install', '_DkzPVuz5eyykUsibtix', '2.0.0');
+</script>
+<?php wp_footer(); ?>
 </body>
+
 </html>
--- header.php
+++ header.php
@@ -23,72 +23,67 @@
 <body <?php body_class(); ?>>
 	<?php wp_body_open(); ?>
 	<a class="visually-hidden-focusable" href="#primary" data-swiftype-index="false"><?php esc_html_e('Skip to content', 'hounslow-intranet-theme'); ?></a>
-
-
-	<div id="before-page" data-swiftype-index="false">
-		<div class="status-bar">
-			<btn class="btn selector"><?php echo do_shortcode('[gtranslate]'); ?></btn>
-			<a id="desktop" class="btn btn-light status-btn" href="/submit-a-new-intranet-page">Add a Page</a>
-			<a id="desktop" class="btn btn-light status-btn" href="/event-submission-form/?action=edit">Add an Event</a>
-		</div>
+	<div class="status-bar" data-swiftype-index="false">
+		<btn class="btn selector"><?php echo do_shortcode('[gtranslate]'); ?></btn>
+		<a id="desktop" class="btn btn-light status-btn" href="/submit-a-new-intranet-page">Add a Page</a>
+		<a id="desktop" class="btn btn-light status-btn" href="/event-submission-form/?action=edit">Add an Event</a>
 	</div>
-	<div id="page" class="site">
-		<header id="masthead" class="site-header" data-swiftype-index="false">
-			<div class="container-fluid">
-				<div class="row align-items-center">
-					<div class="col-lg-2 align-self-center site-branding">
-						<div class="logo-inner">
-							<?php the_custom_logo();
-							if (is_front_page() && is_home()) :
-							else :
-							endif;
-							$test_description = get_bloginfo('description', 'display');
-							if ($test_description || is_customize_preview()) :
-							endif; ?>
-						</div><!-- .logo-inner -->
-					</div><!-- .site-branding -->
-					<div class="col-lg-10">
-						<nav class="navbar navbar-expand-lg navbar-dark">
-							<div class="container-fluid">
-								<button type="button" id="sidebarCollapse" class="btn btn-dark"><i class="fas fa-align-left"></i><span></span></button>
-								<?php if (function_exists('hounslow_intranet_network_nav_menu') && is_user_logged_in()) :
+	<header id="masthead" class="site-header" data-swiftype-index="false">
+		<div class="container-fluid">
+			<div class="row align-items-center">
+				<div class="col-lg-2 align-self-center site-branding">
+					<div class="logo-inner">
+						<?php the_custom_logo();
+						if (is_front_page() && is_home()) :
+						else :
+						endif;
+						$test_description = get_bloginfo('description', 'display');
+						if ($test_description || is_customize_preview()) :
+						endif; ?>
+					</div><!-- .logo-inner -->
+				</div><!-- .site-branding -->
+				<div class="col-lg-10">
+					<nav class="navbar navbar-expand-lg navbar-dark">
+						<div class="container-fluid">
+							<button type="button" id="sidebarCollapse" class="btn btn-dark"><i class="fas fa-align-left"></i><span></span></button>
+							<?php if (function_exists('hounslow_intranet_network_nav_menu') && is_user_logged_in()) :
 
-									$network_menu_args = array(
-										'theme_location'    => 'utility',
-										'depth'             => 2,
-										'container'         => 'div',
-										'container_class'   => 'col-md-6 col-lg',
-										'container_id'      => 'utility-menu',
-										'menu_class'        => 'nav justify-content-end',
-										'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-										'walker'            => new WP_Bootstrap_Navwalker()
-									);
-									hounslow_intranet_network_nav_menu($network_menu_args);
-								elseif (is_user_logged_in()) :
-									$current_user_ID = get_current_user_id();
-									$userdata = get_userdata($current_user_ID);
-									$profile_link = '<a href="' . get_edit_user_link($current_user_ID) . '" class="nav-link" >Welcome, ' . esc_attr($userdata->user_nicename) . '</a>'; ?>
-									<div id="utility-menu" class="col-md-6 col-lg">
-										<ul class="nav justify-content-end">
-											<li class="nav-item username-link">
-												<?php echo $profile_link; ?>
-											</li>
-										</ul>
-									</div><!-- .utility-menu -->
-								<?php else : ?>
-									<div id="utility-menu" class="col-md-6 col-lg">
-										<ul class="nav justify-content-end">
-											<li class="nav-item username-link">
-												<a href="<?php echo wp_login_url(get_permalink()); ?>" class="nav-link">Log In</a>
-											</li>
-										</ul>
-									</div><!-- .utility-menu -->
-								<?php endif;
-								get_search_form(); ?>
-							</div>
-						</nav>
-					</div>
-				</div><!-- .row -->
-			</div><!-- .container -->
-		</header><!-- #masthead -->
-		<div class="wrapper">
+								$network_menu_args = array(
+									'theme_location'    => 'utility',
+									'depth'             => 2,
+									'container'         => 'div',
+									'container_class'   => 'col-md-6 col-lg',
+									'container_id'      => 'utility-menu',
+									'menu_class'        => 'nav justify-content-end',
+									'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+									'walker'            => new WP_Bootstrap_Navwalker()
+								);
+								hounslow_intranet_network_nav_menu($network_menu_args);
+							elseif (is_user_logged_in()) :
+								$current_user_ID = get_current_user_id();
+								$userdata = get_userdata($current_user_ID);
+								$profile_link = '<a href="' . get_edit_user_link($current_user_ID) . '" class="nav-link" >Welcome, ' . esc_attr($userdata->user_nicename) . '</a>'; ?>
+								<div id="utility-menu" class="col-md-6 col-lg">
+									<ul class="nav justify-content-end">
+										<li class="nav-item username-link">
+											<?php echo $profile_link; ?>
+										</li>
+									</ul>
+								</div><!-- .utility-menu -->
+							<?php else : ?>
+								<div id="utility-menu" class="col-md-6 col-lg">
+									<ul class="nav justify-content-end">
+										<li class="nav-item username-link">
+											<a href="<?php echo wp_login_url(get_permalink()); ?>" class="nav-link">Log In</a>
+										</li>
+									</ul>
+								</div><!-- .utility-menu -->
+							<?php endif;
+							get_search_form(); ?>
+						</div>
+					</nav>
+				</div>
+			</div><!-- .row -->
+		</div><!-- .container -->
+	</header><!-- #masthead -->
+	<div class="wrapper">
