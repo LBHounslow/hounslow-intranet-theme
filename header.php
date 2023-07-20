2f16761c498af62bd7f0d623391d2d26b8f2f1b7
Andy Strange <andy.strange@hounslow.gov.uk>
2023-07-20 16:49:42 +0100

    Added One Hounslow logo

--- header.php
+++ header.php
@@ -33,6 +33,7 @@
 			<!-- Branding and Network Home -->
 			<div id="header-row-01-col-01" class="col-lg-2 align-self-center">
 				<div class="logo-inner">
+					<a href="<?php echo network_home_url(); ?>" class="custom-logo-link" rel="home"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/lbh-one-hounslow-logo.png" width="300" height="124" alt="One Hounslow logo" class="custom-logo" /></a>
 				</div><!-- .logo-inner -->
 			</div><!-- .site-branding -->
 			<!-- Utility Navigation -->
