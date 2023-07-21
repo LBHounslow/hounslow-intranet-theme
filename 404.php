42c764306d5c91e5824c28c0e654fbc020b8b790
Andy Strange <andy.strange@hounslow.gov.uk>
2023-07-21 13:28:42 +0100

    Simplified 404 template

--- 404.php
+++ 404.php
@@ -10,25 +10,23 @@
 
 get_header();
 ?>
-<div class="container">
-	<div class="row">
-		<section id="primary" class="content-area col-sm-12 col-lg-8">
-			<main id="main" class="site-main" role="main">
-				<article id="post-error-404" class="error-404 not-found">
-					<header class="entry-header">
-						<h1 class="entry-title">Error 404</h1>
-					</header><!-- .page-header -->
+<!-- Body Main Content -->
+<div id="primary" class="site-main">
+	<article id="post-error-404" class="error-404 not-found">
+		<header class="entry-header">
+			<h1 class="entry-title">Error 404</h1>
+		</header><!-- .page-header -->
 
-					<div class="page-content">
-						<p>Sorry, that page can&rsquo;t be found.</p>
-						<p>The Hounslow Intranet has recently been replaced with a new set of pages - so the content you are looking for may no longer be here.</p>
-						<a href="/" class="btn btn-primary">Back To Home</a>
-						<p>&nbsp;</p>
-					</div><!-- .page-content -->
+		<div class="page-content">
+			<p>Sorry, that page can&rsquo;t be found.</p>
+			<p>The Hounslow Intranet has recently been replaced with a new set of pages - so the content you are looking for may no longer be here.</p>
+			<a href="/" class="btn btn-primary">Back To Home</a>
+			<p>&nbsp;</p>
+		</div><!-- .page-content -->
 
-		</section><!-- .error-404 -->
-		</main><!-- #main -->
-		</section><!-- #primary -->
-		<?php
-		get_sidebar();
-		get_footer();
+	</article>
+
+</div><!-- #primary .site-main -->
+<?php
+get_sidebar();
+get_footer();
