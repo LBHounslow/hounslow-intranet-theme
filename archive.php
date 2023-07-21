d76022395dce3812323ee8a06a4101c533c5e810
Andy Strange <andy.strange@hounslow.gov.uk>
2023-07-21 13:22:27 +0100

    Updated archive template

--- archive.php
+++ archive.php
@@ -8,16 +8,14 @@
  * @package LBH_Intranet_v1
  */
 
-get_header();
-
-
 $url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+get_header();
+?>
+<!-- Body Main Content -->
+<div id="primary" class="site-main">
 
-if (strpos($url, 'corporate-policies') !== false) { ?>
-
-
-  <!-- Page Content  -->
-  <div id="content">
+  <!-- 1) Corporate Policies Archive -->
+  <?php if (strpos($url, 'corporate-policies') !== false) { ?>
 
     <div class="bubble-outer">
       <div class="bubble">
@@ -31,14 +29,10 @@ if (strpos($url, 'corporate-policies') !== false) { ?>
     <div style="padding:20px;background:#83d6c9;margin-bottom:20px;">
       <div class="row align-items-center justify-content-around">
         <div class="col-lg-6">
-
           <?php echo term_description() ?>
         </div>
 
         <div class="col-lg-3">
-
-          <?php // TAXONOMY IMAGE GOES HERE 
-          ?>
         </div>
       </div>
     </div>
@@ -46,80 +40,58 @@ if (strpos($url, 'corporate-policies') !== false) { ?>
       <?php get_template_part('templates/topics-tabs', 'topics-dropdown'); ?>
     </div>
 
+    <div class="row">
+      <div class="col-lg-6">
+        <div class="row">
+          <?php
+          while (have_posts()) :
+            the_post();
 
-    <main id="primary" class="site-main">
-      <div class="row">
-        <div class="col-lg-6">
-          <div class="row">
-            <?php
-            while (have_posts()) :
-              the_post();
+            get_template_part('template-parts/content-post-policy', get_post_type());
 
-              get_template_part('template-parts/content-post-policy', get_post_type());
-
-
-
-            // If comments are open or we have at least one comment, load up the comment template.
-
-
-            endwhile; // End of the loop.
-            ?>
-          </div>
+          endwhile; // End of the loop.
+          ?>
         </div>
       </div>
+    </div>
 
 
-    </main><!-- #main -->
+  <?php } else { ?>
+    <!-- 1) All Other Archives -->
 
+    <?php if (have_posts()) : ?>
 
-  </div>
+      <?php get_template_part('templates/topics-tabs', 'topics-dropdown'); ?>
 
-
-<?php } else { ?>
-
-  <!-- Page Content  -->
-  <div id="content">
-    <?php get_template_part('templates/topics-tabs', 'topics-dropdown'); ?>
-
-    <h3 class="page-title"><?php
-                            printf(__('%s', ''), '<span>' . single_cat_title('', false) . '</span>');
-                            ?></h3>
-
-    <main id="primary" class="site-main">
-      <div class="row">
+      <header class="page-header">
         <?php
-        while (have_posts()) :
-          the_post();
-
-          get_template_part('template-parts/content-topics', get_post_type());
-
-
-
-        // If comments are open or we have at least one comment, load up the comment template.
-
-
-        endwhile; // End of the loop.
+        the_archive_title('<h1 class="page-title">', '</h1>');
+        the_archive_description('<div class="archive-description">', '</div>');
         ?>
+      </header><!-- .page-header -->
+
+      <div class="row">
+      <?php
+      while (have_posts()) :
+        the_post();
+
+        get_template_part('template-parts/content-topics', get_post_type());
+
+      endwhile; // End of the loop.
+
+      the_posts_pagination();
+
+    else :
+
+      get_template_part('template-parts/content', 'none');
+
+    endif;
+      ?>
       </div>
+    <?php } //endif 
+    ?>
 
-
-    </main><!-- #main -->
-
-
-  </div>
-<?php }
-
-?>
-
-
-
-
-</div>
-</body>
-
-
-
-
+</div><!-- #primary .site-main -->
 <?php
 get_sidebar();
 get_footer();
