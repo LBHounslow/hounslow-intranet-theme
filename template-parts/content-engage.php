ed7a2701b23fca67159a6ab6fd3e1dba90bd625a
Andy Strange <andy.strange@hounslow.gov.uk>
2023-07-21 18:22:34 +0100

    Restructured the content-engage template part

--- template-parts/content-engage.php
+++ template-parts/content-engage.php
@@ -8,28 +8,41 @@
  * @package Hounslow_Intranet
  */
 ?>
+<!-- Content Article -->
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<div class="row">
 		<div id="entry-container" class="col-lg-7" style="background:white;">
-			<div id="entry-featured-video">
-				<?php hounslow_intranet_entry_featured_video(); ?>
-			</div>
-			<header class="entry-header">
-				<?php
-				if (is_singular()) :
-					the_title('<h1 class="entry-title">', '</h1>');
-				else :
-					the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
-				endif;
-				?>
+			<!-- Content Header -->
+			<header id="entry-header" class="row">
+				<!-- Content Title -->
+				<div id="entry-title" class="col-12">
+					<?php
+					if (is_singular()) :
+						the_title('<h1 class="entry-title">', '</h1>');
+					else :
+						the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
+					endif;
+					?>
+				</div><!-- #entry-title -->
+				<!-- Content Byline -->
+				<!-- Content Lead -->
+				<div id="entry-lead" class="col-12 entry-summary">
+					<?php hounslow_intranet_entry_lead(); ?>
+				</div><!-- #entry-lead .entry-meta -->
+				<!-- Content Featured Media -->
+				<div id="entry-featured-media" class="col-12 entry-featured-media">
+					<?php hounslow_intranet_entry_featured_video(); ?>
+				</div><!-- #entry-featured-media .entry-featured-media -->
 			</header><!-- .entry-header -->
-			<div class="entry-content">
+			<!-- Content Navigation -->
+			<!-- Content -->
+			<main id="entry-content" class="entry-content">
 				<?php
 				the_content(
 					sprintf(
 						wp_kses(
 							/* translators: %s: Name of current post. Only visible to screen readers */
-							__('Continue reading<span class="screen-reader-text"> "%s"</span>', 'lbh-intranet'),
+							__('Continue reading<span class="screen-reader-text"> "%s"</span>', 'hounslow-intranet'),
 							array(
 								'span' => array(
 									'class' => array(),
@@ -42,28 +55,34 @@
 
 				wp_link_pages(
 					array(
-						'before' => '<div class="page-links">' . esc_html__('Pages:', 'lbh-intranet'),
+						'before' => '<div class="page-links">' . esc_html__('Pages:', 'hounslow-intranet'),
 						'after'  => '</div>',
 					)
 				);
+				?>
+			</main><!-- .entry-content -->
 
-				hounslow_intranet_entry_related_links();
-
+			<!-- Supplementary Content -->
+			<aside id="entry-supplementary-content" class="">
+				<?php hounslow_intranet_entry_related_links(); ?>
+			</aside>
+			<!-- Content Footer -->
+			<footer id="entry-footer" class="entry-footer">
+				<p><?php hounslow_intranet_entry_footer(); ?></p>
+				<?php hounslow_intranet_entry_meta(); ?>
+			</footer><!-- #entry-footer -->
+			<!-- Comments -->
+			<div id="comments-container">
+				<?php
 				// If comments are open or we have at least one comment, load up the comment template.
 				if (comments_open() || get_comments_number()) :
 					comments_template();
 				endif;
 				?>
-			</div><!-- .entry-content -->
-			<footer class="entry-footer">
-				<p><?php hounslow_intranet_entry_footer(); ?></p>
-				<?php hounslow_intranet_entry_meta(); ?>
-			</footer><!-- .entry-footer -->
+			</div>
 		</div>
 		<div class="col-lg-5">
-			<div>
-				<div style="background:url('<?php echo get_the_post_thumbnail_url(); ?>');min-height:100vh;background-size:cover;background-position:center;"></div>
-			</div>
+			<div style="background:url('<?php echo get_the_post_thumbnail_url(); ?>');min-height:100vh;background-size:cover;background-position:center;"></div>
 		</div>
 	</div>
 </article><!-- #post-<?php the_ID(); ?> -->
