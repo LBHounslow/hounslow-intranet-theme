3a350066e1b410b86509637db3494b8c52c03291
Andy Strange <andy.strange@hounslow.gov.uk>
2023-07-26 10:37:49 +0100

    Added tagline for events page

--- template-parts/page-bubble.php
+++ template-parts/page-bubble.php
@@ -8,10 +8,15 @@
 <div style="padding:20px;background:#83d6c9;margin-bottom:20px;">
     <div class="row align-items-center justify-content-around">
         <div class="col-lg-6">
-        <?php if (have_posts()) : while (have_posts()) : the_post();
-                    the_content();
-                endwhile;
-            endif; ?>
+            <?php
+            if (is_page('events')) {
+                echo '<p>Find out what is happening at the Council, including events and social opportunities.</p>';
+            } else {
+                if (have_posts()) : while (have_posts()) : the_post();
+                        the_content();
+                    endwhile;
+                endif;
+            } ?>
         </div>
         <div class="col-lg-3">
             <?php $images = rwmb_meta('second_image', array('size' => 'full'));
