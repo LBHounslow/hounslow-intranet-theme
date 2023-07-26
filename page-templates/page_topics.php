671f8bc085c32cb4448f2409413d289a6cd6f41c
Andy Strange <andy.strange@hounslow.gov.uk>
2023-07-26 09:29:41 +0100

    Reorganised the topics archives template parts

--- page-templates/page_topics.php
+++ page-templates/page_topics.php
@@ -18,25 +18,25 @@ get_header();
   $url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
 
   if (strpos($url, 'working-together') !== false) {
-    get_template_part('templates/topics/working-together-archive', 'wt');
+    get_template_part('template-parts/topics/working-together-archive', 'wt');
   } else if (strpos($url, 'health-and-wellbeing') !== false) {
-    get_template_part('templates/topics/health-and-wellbeing-archive', 'haw');
+    get_template_part('template-parts/topics/health-and-wellbeing-archive', 'haw');
   } else if (strpos($url, 'new-to-lbh') !== false) {
     echo '';
   } else if (strpos($url, 'get-involved') !== false) {
-    get_template_part('templates/topics/get-involved-archive', 'gi');
+    get_template_part('template-parts/topics/get-involved-archive', 'gi');
   } else if (strpos($url, 'events') !== false) {
-    get_template_part('templates/topics/events-archive', 'es');
+    get_template_part('template-parts/topics/events-archive', 'es');
   } else if (strpos($url, 'develop-and-learn') !== false) {
-    get_template_part('templates/topics/develop-and-learn-archive', 'es');
+    get_template_part('template-parts/topics/develop-and-learn-archive', 'es');
   } else if (strpos($url, 'how-do-i') !== false) {
-    get_template_part('templates/topics/how-do-i-archive', 'hdi');
+    get_template_part('template-parts/topics/how-do-i-archive', 'hdi');
   } else if (strpos($url, 'corporate-policies') !== false) {
-    get_template_part('templates/topics/corp-policy-archive', 'p');
+    get_template_part('template-parts/topics/corp-policy-archive', 'p');
   } else if (strpos($url, 'world-of-work') !== false) {
-    get_template_part('templates/topics/world-of-work-archive', 'wow');
+    get_template_part('template-parts/topics/world-of-work-archive', 'wow');
   } else if (strpos($url, 'one-hounslow') !== false) {
-    get_template_part('templates/topics/one-hounslow-archive', 'oh');
+    get_template_part('template-parts/topics/one-hounslow-archive', 'oh');
   } ?>
 
   <style>
