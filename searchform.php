5498f8db3fa9053be27aaba61f3ce4d96d34fe68
Andy Strange <andy.strange@hounslow.gov.uk>
2023-09-21 13:58:22 +0100

    Redesign of search form (use of WordPress core)

--- searchform.php
+++ searchform.php
@@ -1,8 +1,7 @@
-<form role="search" method="get" class="form search-form" action="/search-results/">
-  <div class="input-group">
-      <input name="s" type="text" class="st-default-search-input" placeholder="Search the Intranet">
-      <span class="input-group-btn">
-        <button type="submit" value="Search" class="btn btn-dark" type="button"><i class="fa fa-search" aria-hidden="true"></i>&nbsp;</button>
-      </span>
-  </div>
-</form>
+<nav class="navbar navbar-expand-lg navbar-light">
+    <form class="container-fluid" role="search" method="get" action="<?php echo home_url('/'); ?>">
+        <input class="form-control me-2" type="search" placeholder="<?php echo esc_attr_x('Search …', 'placeholder') ?>" aria-label="Search" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x('Search for:', 'label') ?>">
+        <button class="btn btn-primary me-2" type="submit" value="<?php echo esc_attr_x('Search', 'submit button') ?>"><i class="fa fa-search"></i></button>
+        <a href=""><span class="btn btn-outline-primary"><i class="fas fa-search-plus"></i></span></a>
+    </form>
+</nav>
