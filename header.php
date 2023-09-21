014c40087c22e0a1f5e4cc982786683e206a77a4
Andy Strange <andy.strange@hounslow.gov.uk>
2023-09-21 10:54:05 +0100

    Organise header elements into template parts

--- header.php
+++ header.php
@@ -27,51 +27,12 @@
 		<div id="header-row-01" class="row">
 			<!-- Branding and Network Home -->
 			<div id="header-row-01-col-01" class="col-lg-2 align-self-center">
-				<div class="logo-inner">
-					<a href="<?php echo network_home_url(); ?>" class="custom-logo-link" rel="home"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/lbh-one-hounslow-logo.png" width="300" height="124" alt="One Hounslow logo" class="custom-logo" /></a>
-				</div><!-- .logo-inner -->
-			</div><!-- .site-branding -->
+				<?php get_template_part('template-parts/header', 'branding'); ?>
+			</div><!-- #header-row-01-col-01 -->
 			<!-- Utility Navigation -->
 			<div id="header-row-01-col-02" class="col-lg-10">
-				<nav class="navbar navbar-expand-lg navbar-dark">
-					<div class="container-fluid">
-						<?php if (function_exists('hounslow_intranet_network_nav_menu') && is_user_logged_in()) :
-
-							$network_menu_args = array(
-								'theme_location'    => 'utility',
-								'depth'             => 2,
-								'container'         => 'div',
-								'container_class'   => 'col-md-6 col-lg',
-								'container_id'      => 'utility-menu',
-								'menu_class'        => 'nav justify-content-end',
-								'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-								'walker'            => new WP_Bootstrap_Navwalker()
-							);
-							hounslow_intranet_network_nav_menu($network_menu_args);
-						elseif (is_user_logged_in()) :
-							$current_user_ID = get_current_user_id();
-							$userdata = get_userdata($current_user_ID);
-							$profile_link = '<a href="' . get_edit_user_link($current_user_ID) . '" class="nav-link" >Welcome, ' . esc_attr($userdata->user_nicename) . '</a>'; ?>
-							<div id="utility-menu" class="col-md-6 col-lg">
-								<ul class="nav justify-content-end">
-									<li class="nav-item username-link">
-										<?php echo $profile_link; ?>
-									</li>
-								</ul>
-							</div><!-- .utility-menu -->
-						<?php else : ?>
-							<div id="utility-menu" class="col-md-6 col-lg">
-								<ul class="nav justify-content-end">
-									<li class="nav-item username-link">
-										<a href="<?php echo wp_login_url(get_permalink()); ?>" class="nav-link">Log In</a>
-									</li>
-								</ul>
-							</div><!-- .utility-menu -->
-						<?php endif;
-						get_search_form(); ?>
-					</div>
-				</nav>
-			</div>
+				<?php get_template_part('template-parts/header', 'utilitynav'); ?>
+			</div><!-- #header-row-01-col-02 -->
 		</div><!-- #header-row-01 .row -->
 	</div><!-- #header .container  -->
 	<div class="wrapper">
---
+++ template-parts/header-utilitynav.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-lg navbar-dark">
+    <div class="container-fluid">
+        <?php if (function_exists('hounslow_intranet_network_nav_menu') && is_user_logged_in()) :
+
+            $network_menu_args = array(
+                'theme_location'    => 'utility',
+                'depth'             => 2,
+                'container'         => 'div',
+                'container_class'   => 'col-md-6 col-lg',
+                'container_id'      => 'utility-menu',
+                'menu_class'        => 'nav justify-content-end',
+                'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                'walker'            => new WP_Bootstrap_Navwalker()
+            );
+            hounslow_intranet_network_nav_menu($network_menu_args);
+        elseif (is_user_logged_in()) :
+            $current_user_ID = get_current_user_id();
+            $userdata = get_userdata($current_user_ID);
+            $profile_link = '<a href="' . get_edit_user_link($current_user_ID) . '" class="nav-link" >Welcome, ' . esc_attr($userdata->user_nicename) . '</a>'; ?>
+            <div id="utility-menu" class="col-md-6 col-lg">
+                <ul class="nav justify-content-end">
+                    <li class="nav-item username-link">
+                        <?php echo $profile_link; ?>
+                    </li>
+                </ul>
+            </div><!-- .utility-menu -->
+        <?php else : ?>
+            <div id="utility-menu" class="col-md-6 col-lg">
+                <ul class="nav justify-content-end">
+                    <li class="nav-item username-link">
+                        <a href="<?php echo wp_login_url(get_permalink()); ?>" class="nav-link">Log In</a>
+                    </li>
+                </ul>
+            </div><!-- .utility-menu -->
+        <?php endif;
+        get_search_form(); ?>
+    </div>
+</nav>
