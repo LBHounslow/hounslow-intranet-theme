a184a4e0008d920bb6975c3bc11e6510ec3c0202
Andy Strange <andy.strange@hounslow.gov.uk>
2023-10-06 12:44:36 +0100

    Add post date to search results

--- template-parts/search-entry.php
+++ template-parts/search-entry.php
@@ -3,6 +3,11 @@
         <div class="row">
             <div class="col-10">
                 <?php the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>'); ?>
+                <div class="mb-2">
+                    <?php if (hounslow_intranet_get_post_type() == 'post') {
+                        hounslow_intranet_posted_on();
+                    } ?>
+                </div>
             </div>
             <div class="col-2 p-2 d-flex justify-content-center">
                 <span><?php echo hounslow_intranet_post_type_identifier(); ?></span>
