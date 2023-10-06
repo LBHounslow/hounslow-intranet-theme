bf494456fcff88feaf1dfa44049e355bcfcde2b6
Andy Strange <andy.strange@hounslow.gov.uk>
2023-10-06 08:33:37 +0100

    Remove custom header feature

--- functions.php
+++ functions.php
@@ -219,11 +219,6 @@ add_action('wp_enqueue_scripts', 'hounslow_intranet_scripts');
 require get_template_directory() . '/inc/utility-functions.php';
 
 /**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
