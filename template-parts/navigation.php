087aef56bb8f68518d445402aee56891b967a2b1
Andy Strange <andy.strange@hounslow.gov.uk>
2023-10-18 10:29:28 +0100

    Hide sidebar menus if not set

--- template-parts/navigation.php
+++ template-parts/navigation.php
@@ -1,61 +1,73 @@
 <?php if (function_exists('hounslow_intranet_network_nav_menu')) : ?>
 
-	<div class="sidebar-header">
-		<h5>Engage</h5>
-	</div>
+	<?php if (has_nav_menu('engage')) : ?>
 
-	<nav id="side-bar" class="side-bar-menu">
-		<?php
-		$network_menu_args = array(
-			'theme_location'    => 'engage',
-			'fallback_cb'		=> false,
-		);
-		hounslow_intranet_network_nav_menu($network_menu_args);
-		?>
-	</nav><!-- #site-navigation -->
-
-	<div class="sidebar-header">
-		<h5>Popular Links</h5>
-	</div>
-
-	<nav id="side-bar" class="side-bar-menu">
-		<?php
-		$network_menu_args = array(
-			'theme_location'    => 'popular',
-			'fallback_cb'				=> false,
-		);
-		hounslow_intranet_network_nav_menu($network_menu_args);
-		?>
-	</nav><!-- #site-navigation -->
-
-	<div class="sidebar-header">
-		<h5>Connect</h5>
-	</div>
-
-	<nav id="side-bar" class="side-bar-menu">
-		<?php
-		$network_menu_args = array(
-			'theme_location'    => 'support',
-			'fallback_cb'				=> false,
-		);
-		hounslow_intranet_network_nav_menu($network_menu_args);
-		?>
-	</nav><!-- #site-navigation -->
-
-	<div class="sidebar-out-links">
 		<div class="sidebar-header">
-			<h5>External Websites</h5>
+			<h5>Engage</h5>
 		</div>
+
 		<nav id="side-bar" class="side-bar-menu">
 			<?php
 			$network_menu_args = array(
-				'theme_location'    => 'external',
+				'theme_location'    => 'engage',
+				'fallback_cb'		=> false,
+			);
+			hounslow_intranet_network_nav_menu($network_menu_args);
+			?>
+		</nav><!-- #site-navigation -->
+
+	<?php endif;
+	if (has_nav_menu('popular')) : ?>
+
+		<div class="sidebar-header">
+			<h5>Popular Links</h5>
+		</div>
+
+		<nav id="side-bar" class="side-bar-menu">
+			<?php
+			$network_menu_args = array(
+				'theme_location'    => 'popular',
 				'fallback_cb'				=> false,
 			);
 			hounslow_intranet_network_nav_menu($network_menu_args);
 			?>
 		</nav><!-- #site-navigation -->
-	</div>
+
+	<?php endif;
+	if (has_nav_menu('support')) : ?>
+
+		<div class="sidebar-header">
+			<h5>Connect</h5>
+		</div>
+
+		<nav id="side-bar" class="side-bar-menu">
+			<?php
+			$network_menu_args = array(
+				'theme_location'    => 'support',
+				'fallback_cb'				=> false,
+			);
+			hounslow_intranet_network_nav_menu($network_menu_args);
+			?>
+		</nav><!-- #site-navigation -->
+
+	<?php endif;
+	if (has_nav_menu('external')) : ?>
+
+		<div class="sidebar-out-links">
+			<div class="sidebar-header">
+				<h5>External Websites</h5>
+			</div>
+			<nav id="side-bar" class="side-bar-menu">
+				<?php
+				$network_menu_args = array(
+					'theme_location'    => 'external',
+					'fallback_cb'				=> false,
+				);
+				hounslow_intranet_network_nav_menu($network_menu_args);
+				?>
+			</nav><!-- #site-navigation -->
+		</div>
+	<?php endif; ?>
 <?php else : ?>
 	<nav id="side-bar" class="side-bar-menu">
 		<?php
