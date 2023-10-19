03048a8bc2731a5f9c699e5cb646a2b2bad59454
Andy Strange <andy.strange@hounslow.gov.uk>
2023-10-19 14:01:20 +0100

    Update header-networknav.php

--- template-parts/header-networknav.php
+++ template-parts/header-networknav.php
@@ -1,5 +1,5 @@
 <?php
-if (has_nav_menu('network') && function_exists('hounslow_intranet_network_nav_menu')) :
+if (function_exists('hounslow_intranet_network_nav_menu')) :
     $network_menu_args = array(
         'theme_location'    => 'network',
         'depth'             => 2,
@@ -7,7 +7,7 @@ if (has_nav_menu('network') && function_exists('hounslow_intranet_network_nav_me
         'container_class'   => '',
         'container_id'      => '',
         'menu_class'        => 'nav nav-fill',
-        'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+        'fallback_cb'        => false,
         'walker'            => new WP_Bootstrap_Navwalker()
     );
 ?>
